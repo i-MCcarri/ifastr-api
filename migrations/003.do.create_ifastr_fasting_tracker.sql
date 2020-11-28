22149ba8d0c86938ed4c50de34722ee1b18958b7
Ady <ady@gms-macbook-pro.lan>
2020-11-27 16:30:00 -0800

    implemented post to tracker

--- migrations/003.do.create_ifastr_fasting_tracker.sql
+++ migrations/003.do.create_ifastr_fasting_tracker.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS fasting_tracker;
 CREATE TABLE fasting_tracker
 (
     fasting_id      SERIAL PRIMARY KEY,
-    fasting_start   TIMESTAMPTZ,
+    fasting_start   TIME,
     fasting_length  INTEGER,
     feast_start     TIMESTAMPTZ DEFAULT now() NOT NULL,
     completed       BOOLEAN DEFAULT false
